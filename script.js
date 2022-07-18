5b486f049d8f45ea0fcc3d9b78ce151d3311eb9a
mueez-uddin <86313423+mueez-uddin@users.noreply.github.com>
2022-07-18 10:16:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+   document.getElementById("Drop-Down").onclick = function myFunction(){
+    document.getElementById("Lis-Lists").classList.toggle("Active");
+  };
+
+  document.getElementById("DarkMood").onclick = function darkFnction() {
+    document.body.classList.toggle('Dark_Mood')
+  }
